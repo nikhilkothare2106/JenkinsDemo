247081740e922b853d406946aabbc31dc518c65a
Nikhil Kothare <nikhilkothare12@gmail.com>
2025-06-10 00:12:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Date;
+
 public class Main{
   public static void main(String[] args){
     System.out.println("Jenkins Program");
     System.out.println("Hello !!!");
+     System.out.println("Date is: " + new Date());
   }
 }
