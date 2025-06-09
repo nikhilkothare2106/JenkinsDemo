53b00988980f04d31d6a2b214666ae9d777e76f7
Nikhil Kothare <nikhilkothare12@gmail.com>
2025-06-10 00:09:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
     System.out.println("Jenkins Program");
-    System.oout.println("Hello !!!");
+    System.out.println("Hello !!!");
   }
 }
